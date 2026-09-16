854940cdabff0b7689116180310a9d769fe4d52d
elisha gerson <elishagerson2@gmail.com>
2026-09-16 11:46:30 +0300

    configured server to use routers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,18 @@
 import express from "express";
+import cors from "cors";
 const PORT = 5000;
 const app = express();
+import usersRouter from "./routes/users";
 
+app.use(cors());
 app.use(express.json());
+app.use("/api/v1/users", usersRouter);
 
 app.get("/", (req, res) => {
     res.json({
         message: "Backend is running"
     });
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
