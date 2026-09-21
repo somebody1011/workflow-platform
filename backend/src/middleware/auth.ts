6d74a8cc3672127cb15b6c4dd68983ab0e90e742
elisha gerson <elishagerson2@gmail.com>
2026-09-21 10:58:43 +0300

    implemented token verification for authorizaion

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,4 +1,4 @@
-import type{ Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export interface AuthRequest extends Request {
